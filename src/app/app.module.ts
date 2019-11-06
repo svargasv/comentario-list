0176d7f3c74baaa33fa83370384599933e65adbc
svargasv <s.vargasv@uniandes.edu.co>
2019-11-06 17:16:10 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import {ComentarioModule} from './comentario/comentario.module';
 import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,ComentarioModule,HttpClientModule],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule,ComentarioModule,HttpClientModule,AppRoutingModule],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
