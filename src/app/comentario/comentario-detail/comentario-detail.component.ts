c41b8ccb269ca782591cf411941dd0e1d73d07eb
svargasv <s.vargasv@uniandes.edu.co>
2019-11-07 07:36:36 -0500

    4 files modified, 1 deleted

--- src/app/comentario/comentario-detail/comentario-detail.component.ts
+++ src/app/comentario/comentario-detail/comentario-detail.component.ts
@@ -6,7 +6,6 @@ import{Comentario} from '../comentario';
 @Component({
   selector: 'app-comentario-detail',
   templateUrl: './comentario-detail.component.html',
-  styleUrls: ['./comentario-detail.component.css']
 })
 export class ComentarioDetailComponent implements OnInit {
 
